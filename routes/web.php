4b40979cc10d022e25646c4841e487b504de232d
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-05-27 17:59:37 -0500

    template dark

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/', 'HomeController@dashboard')->name('dashboard');
+
