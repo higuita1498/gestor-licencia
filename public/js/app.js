685b7ab6e48ed5e708f5011284c00af068b4ecbc
Juan Esteban <juanes0202@gmail.com>
2022-06-04 22:06:19 -0500

    creando seeder de usuarios para el admin

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\gestor-licencia\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\gestor-licencia\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Juankno\Proyectos\gestor-licencia\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Juankno\Proyectos\gestor-licencia\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
