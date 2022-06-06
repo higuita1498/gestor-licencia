00c94b0463a83c9769eddd1a91deb5f8d62949cb
Juan Esteban <juanes0202@gmail.com>
2022-06-06 17:53:36 -0500

    creando submenu de usuarios

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -5,37 +5,51 @@
             <a href="#" class="simple-text logo-normal">{{ __('Black Dashboard') }}</a>
         </div>
         <ul class="nav">
-            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+            <li @if ($pageSlug=='dashboard' ) class="active " @endif>
                 <a href="{{ route('home') }}">
                     <i class="tim-icons icon-chart-pie-36"></i>
                     <p>{{ __('Dashboard') }}</p>
                 </a>
             </li>
-            <li @if ($pageSlug == 'icons') class="active " @endif>
+            <li @if ($pageSlug=='icons' ) class="active " @endif>
                 <a href="javascript:void(0)">
                     <i class="tim-icons icon-badge"></i>
                     <p>Generar licencias</p>
                 </a>
             </li>
-            <li @if ($pageSlug == 'maps') class="active " @endif>
+            <li @if ($pageSlug=='maps' ) class="active " @endif>
                 <a href="javascript:void(0)">
                     <i class="tim-icons icon-key-25"></i>
                     <p>Generar Keys</p>
                 </a>
             </li>
-            <li @if ($pageSlug == 'users') class="active " @endif>
-                <a href="{{ route('users.index') }}">
+
+            <li>
+                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="{{ $pageSlug == 'users' ?  true : false }}">
                     <i class="tim-icons icon-single-02"></i>
-                    <p>Usuarios</p>
+                    <span class="nav-link-text">Gestión de Usuarios</span>
+                    <b class="caret mt-1"></b>
                 </a>
+
+                <div class="{{ $pageSlug == 'users' ?  'collapse show' : 'collapse' }}" id="laravel-examples">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
+                            <a href="{{ route('users.index') }}">
+                                <i class="tim-icons icon-single-02"></i>
+                                <p>Usuarios</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
-            <li @if ($pageSlug == 'maps') class="active " @endif>
+
+            <li @if ($pageSlug=='maps' ) class="active " @endif>
                 <a href="javascript:void(0)">
                     <i class="tim-icons icon-single-02"></i>
                     <p>Socios</p>
                 </a>
             </li>
-            <li @if ($pageSlug == 'maps') class="active " @endif>
+            <li @if ($pageSlug=='maps' ) class="active " @endif>
                 <a href="javascript:void(0)">
                     <i class="tim-icons icon-settings"></i>
                     <p>Configuración</p>
