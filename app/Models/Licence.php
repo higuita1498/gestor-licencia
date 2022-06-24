b8c3e05e5f7e1370e9dc7742588e3b019c2f768e
SantiagoSierra <48597889+SantiagoSierra@users.noreply.github.com>
2022-06-24 18:34:44 -0500

    Update Licence.php

--- app/Models/Licence.php
+++ app/Models/Licence.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Product;
+use App\Partner;
+
 
 class Licence extends Model
 {
     //
     protected $fillable = ['LicenseKey', 'ProductID', 'product_id', 'Status', 'PartnerID', 'partner_id', 'MasterCode', 'UserID', 'user_id'];
 
-    
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public function partner(){
+        return $this->belongsTo(Partner::class);
+    }
 
 }
