bb7f0f9edcc436d869fbd2eab82541fa1ac49b35
Juan Esteban <juanes0202@gmail.com>
2022-06-25 09:58:35 -0500

    wip

--- resources/views/licences/create.blade.php
+++ resources/views/licences/create.blade.php
@@ -20,7 +20,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="title">Crear usuario</h5>
+                    <h5 class="title">Crear licencia</h5>
                 </div>
                 <div class="card-body">
                     <div class="row">
