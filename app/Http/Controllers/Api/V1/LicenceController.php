40e2e6579fcc4622dabbe8bc6d989bd3891fe590
SantiagoSierra <48597889+SantiagoSierra@users.noreply.github.com>
2022-06-30 22:52:50 -0500

    structura simple de respuesta JSON

--- app/Http/Controllers/Api/V1/LicenceController.php
+++ app/Http/Controllers/Api/V1/LicenceController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use App\Models\Licence;
 use Illuminate\Http\Request;
+use App\Models\Licence;
+use App\Models\Partner;
+use App\Models\Product;
+use App\Http\Requests\StoreLicenceRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class LicenceController extends Controller
 {
@@ -13,9 +17,18 @@ class LicenceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($skip = 0, $take = 100)
     {
         //
+        $licences = Licence::with('product', 'partner', 'user')->skip($skip)->take($take)->get();
+
+        return response()->json(['success' => true,
+                                 'message' => 'Data found',
+                                 'data' => $licences->all(),
+                                 'skip' => $skip,
+                                 'take' => $take,
+                                 'type' => 'array'
+        ]);
     }
 
     /**
@@ -26,7 +39,42 @@ class LicenceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+
+            DB::beginTransaction();
+
+            $product = Product::find($request->product_id);
+            $partner = Partner::find($request->partner_id);
+
+            for ($i = 0; $i < $request->licencesNumber; $i++) {
+                $licence = Licence::create([
+                    'LicenseKey' => Str::uuid(),
+                    'ProductID' => $product->IdProduct,
+                    'product_id' => $product->id,
+                    'PartnerID' => $partner->PartnerID,
+                    'partner_id' => $partner->id,
+                ]);
+            }
+
+            DB::commit();
+
+
+            $response = response()->json(['success' => true,
+                                    'message' => 'Licencia registrada correctamente',
+                                    'data' => $licence,
+                                    'type' => 'object'
+            ]);
+
+        } catch (\Throwable $th) {
+
+            $response = response()->json(['success' => false,
+                                     'message' => 'La licencia no se pudo crear',
+                                     'data' => $th->getMessage(),
+                                     'type' => 'object'
+            ]);
+        }
+
+        return $response;
     }
 
     /**
@@ -38,6 +86,22 @@ class LicenceController extends Controller
     public function show(Licence $licence)
     {
         //
+        if($licence){
+            $licence->load('product', 'partner', 'user');
+
+            return response(['success' => true,
+                             'message' => 'Licencia encontrada',
+                             'data' => $licence,
+                             'type' => 'object'
+                            ]);
+        }else{
+            return response(['success' => false,
+                             'message' => 'No se encontro la licencia',
+                             'data' => null,
+                             'type' => 'object'
+            ]);
+        }
+
     }
 
     /**
